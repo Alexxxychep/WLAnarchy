7862b103391ddf78f906fc8b98641450d06ebf7e
Alexxxychep <Alexeychepurn@gmail.com>
2026-08-03 02:50:16 +0300

    Async chat logic

--- src/main/java/me/alexxxychep/wlanarchy/chat/ChatListener.java
+++ src/main/java/me/alexxxychep/wlanarchy/chat/ChatListener.java
@@ -1,5 +1,6 @@
 package me.alexxxychep.wlanarchy.chat;
 
+import io.papermc.paper.chat.ChatRenderer;
 import io.papermc.paper.event.player.AsyncChatDecorateEvent;
 import io.papermc.paper.event.player.AsyncChatEvent;
 import org.bukkit.event.EventHandler;
@@ -10,10 +11,16 @@ import java.util.UUID;
 
 @Singleton
 public class ChatListener implements Listener {
+    ChatMessageFactory messageFactory = new ChatMessageFactory();
+
     @EventHandler
     public void onAsyncChat(AsyncChatEvent event) {
         UUID uuid = event.getPlayer().getUniqueId();
-
+        WLChatMessage chatMessage = messageFactory.fromComponent(event.message());
+        event.renderer((source, sourceDisplayName, message, viewer) -> {
+            
+            return message;
+        });
     }
 
     @EventHandler
--- src/main/java/me/alexxxychep/wlanarchy/chat/ChatMessageFactory.java
+++ src/main/java/me/alexxxychep/wlanarchy/chat/ChatMessageFactory.java
@@ -2,7 +2,10 @@ package me.alexxxychep.wlanarchy.chat;
 
 import net.kyori.adventure.text.Component;
 
+import javax.inject.Singleton;
+
 public class ChatMessageFactory {
+
     public WLChatMessage fromComponent(Component component) {
 
         return new WLChatMessage(true, component);
