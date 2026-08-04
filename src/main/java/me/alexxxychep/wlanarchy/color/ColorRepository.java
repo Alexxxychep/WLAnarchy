7bd13a1190b835c9a97ed5c9a9ec69fe07be3d09
Alexxxychep <Alexeychepurn@gmail.com>
2026-08-04 03:22:29 +0300

    Base classes for upcoming message color feature

---
+++ src/main/java/me/alexxxychep/wlanarchy/color/ColorRepository.java
@@ -0,0 +1,4 @@
+package me.alexxxychep.wlanarchy.color;
+
+public class ColorRepository {
+}
---
+++ src/main/java/me/alexxxychep/wlanarchy/color/ColorService.java
@@ -0,0 +1,10 @@
+package me.alexxxychep.wlanarchy.color;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class ColorService {
+    public HashMap<UUID, ColorPalette> palettesCache;
+
+    public v
+}
